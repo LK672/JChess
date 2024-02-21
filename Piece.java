d8dc5285e4e2439bb684ae6ea3b959a7f3b1605f
LK672 <144760164+LK672@users.noreply.github.com>
2024-02-21 02:05:28 -0500

    FEN array conversion pt1
    Implemented FEN to array and array to FEN conversion but not working as intended. Must fix next time. After that probably start working on moves/player logic

--- Piece.java
+++ Piece.java
@@ -1,33 +1,40 @@
 package JChess;
 import java.awt.*;
-
 import javax.swing.ImageIcon;
 
 public final class Piece {
 
-    public static int None = 0;
-    public static int King = 1;
-    public static int Pawn = 2;
-    public static int Knight = 3;
-    public static int Bishop = 4;
-    public static int Rook = 5;
-    public static int Queen = 6;
+    public static enum Pieces {
+        
+        NONE,
+        BKING,
+        BPAWN,
+        BKNIGHT,
+        BBISHOP,
+        BROOK,
+        BQUEEN,
 
-    public static int White = 8;
-    public static int Black = 16;
+        WKING,
+        WPAWN,
+        WKNIGHT,
+        WBISHOP,
+        WROOK,
+        WQUEEN,
 
-    public static Image BB = new ImageIcon("JChess/BlackBishop.png").getImage();
-    public static Image BKI = new ImageIcon("JChess/BlackKing.png").getImage();
-    public static Image BKN = new ImageIcon("JChess/BlackKnight.png").getImage();
-    public static Image BP = new ImageIcon("JChess/BlackPawn.png").getImage();
-    public static Image BQ = new ImageIcon("JChess/BlackQueen.png").getImage();
-    public static Image BR = new ImageIcon("JChess/BlackRook.png").getImage();
+    }
 
-    public static Image WB = new ImageIcon("JChess/WhiteBishop.png").getImage();
-    public static Image WKI = new ImageIcon("JChess/WhiteKing.png").getImage();
-    public static Image WKN = new ImageIcon("JChess/WhiteKnight.png").getImage();
-    public static Image WP = new ImageIcon("JChess/WhitePawn.png").getImage();
-    public static Image WQ = new ImageIcon("JChess/WhiteQueen.png").getImage();
-    public static Image WR = new ImageIcon("JChess/WhiteRook.png").getImage();
+    public static Image BB = new ImageIcon("JChess/Images/BlackBishop.png").getImage();
+    public static Image BKI = new ImageIcon("JChess/Images/BlackKing.png").getImage();
+    public static Image BKN = new ImageIcon("JChess/Images/BlackKnight.png").getImage();
+    public static Image BP = new ImageIcon("JChess/Images/BlackPawn.png").getImage();
+    public static Image BQ = new ImageIcon("JChess/Images/BlackQueen.png").getImage();
+    public static Image BR = new ImageIcon("JChess/Images/BlackRook.png").getImage();
+
+    public static Image WB = new ImageIcon("JChess/Images/WhiteBishop.png").getImage();
+    public static Image WKI = new ImageIcon("JChess/Images/WhiteKing.png").getImage();
+    public static Image WKN = new ImageIcon("JChess/Images/WhiteKnight.png").getImage();
+    public static Image WP = new ImageIcon("JChess/Images/WhitePawn.png").getImage();
+    public static Image WQ = new ImageIcon("JChess/Images/WhiteQueen.png").getImage();
+    public static Image WR = new ImageIcon("JChess/Images/WhiteRook.png").getImage();
     
 }
